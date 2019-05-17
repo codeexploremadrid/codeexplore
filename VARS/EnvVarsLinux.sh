84b70cd9e6ee4ab4d021f4a4d1cb09e1cdffc2cc
Oracle Code Explore Madrid 2019 <50697046+codeexploremadrid@users.noreply.github.com>
2019-05-17 23:29:51 +0200

    Update EnvVarsLinux.sh

--- VARS/EnvVarsLinux.sh
+++ VARS/EnvVarsLinux.sh
@@ -1,9 +1,9 @@
 #### VARIABLES DE ENTORNO PARA TERRAFORM ENTORNO CODEEXPLORE ######
-export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaarq2oww6u6ah4hu3cynqxkjhsz7ckwb2dagxbp6xs66xui3v547bq"
-export TF_VAR_compartment_ocid="ocid1.compartment.oc1..aaaaaaaahfrxab5ufpqqzrjyqo7oy57f67fchwaojyvnz2wg2wytshpmtelq"
+export TF_VAR_user_ocid=""
+export TF_VAR_compartment_ocid=""
 export TF_VAR_compartment_name="codeexplore"
-export TF_VAR_api_fingerprint="77:36:b2:3b:6e:3b:45:0e:fa:e7:9c:ea:3d:0e:50:f0"
+export TF_VAR_api_fingerprint=""
 export TF_VAR_region="eu-frankfurt-1"
-export TF_VAR_tenancy="jguerra"
-export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaa4iny3ikkjrbibay6rxu7neuggb3n5vptzgxgoo3t42h3abdl3ufa"
+export TF_VAR_tenancy=""
+export TF_VAR_tenancy_ocid=""
 ###############################################
