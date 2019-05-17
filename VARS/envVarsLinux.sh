4724adffca78f15e59db0241bd58ecdc80edb661
Oracle Code Explore Madrid 2019 <50697046+codeexploremadrid@users.noreply.github.com>
2019-05-18 00:58:26 +0200

    Create envVarsLinux.sh

---
+++ VARS/envVarsLinux.sh
@@ -0,0 +1,8 @@
+#### VARIABLES DE ENTORNO PARA TERRAFORM ENTORNO CODEEXPLORE ######
+export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaa7dpdau7iq4oq3bfrqkaxf6rldwv4xu2gkmnl4344h7vyydjo6ima"
+export TF_VAR_compartment_ocid="ocid1.compartment.oc1..aaaaaaaaejlswyiy2uhqgvrshqzivduye5vlo24i2j6t4qjtb4v5h3p5k6ba"
+export TF_VAR_compartment_name="codeexplore"
+export TF_VAR_fingerprint="77:36:b2:3b:6e:3b:45:0e:fa:e7:9c:ea:3d:0e:50:f0"
+export TF_VAR_region="eu-frankfurt-1"
+export TF_VAR_tenancy="enimbosdos"
+export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaaif5jkjgrzztb7vj4zonc7kvynba3zooyw3lzgtaj57idl5js2rca"
