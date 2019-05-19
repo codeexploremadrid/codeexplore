935665974ca3695163533e02db266bbeac138ba3
Oracle Code Explore Madrid 2019 <50697046+codeexploremadrid@users.noreply.github.com>
2019-05-19 18:48:50 +0200

    Update createPDB2.sql

--- DBCS/createPDB2.sql
+++ DBCS/createPDB2.sql
@@ -1,5 +1,9 @@
 create pluggable database PDB2 ADMIN USER pdb_adm identified by "ZZ0r_cle#1";
 alter pluggable database PDB2 open;
-alter session set container=PDB2;
+ADMINISTER KEY MANAGEMENT SET KEYSTORE close;
+ADMINISTER KEY MANAGEMENT SET KEYSTORE open IDENTIFIED BY "ZZ0r_cle#1" CONTAINER=all;
+ALTER SESSION SET CONTAINER = pdb2;
+ADMINISTER KEY MANAGEMENT SET KEY USING TAG 'tag' IDENTIFIED BY "ZZ0r_cle#1" WITH BACKUP USING 'backup_identifier';
 create tablespace TBS2 datafile size 1G;
 select name from dba_services;
+ALTER SESSION SET CONTAINER = CDB$root;
