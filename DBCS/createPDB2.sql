c65c39e0e4de5a36716a4aecf6d458d1322e7664
Oracle Code Explore Madrid 2019 <50697046+codeexploremadrid@users.noreply.github.com>
2019-05-19 18:14:46 +0200

    Update createPDB2.sql

--- DBCS/createPDB2.sql
+++ DBCS/createPDB2.sql
@@ -1,4 +1,4 @@
-create pluggable database PDB2 ADMIN USER pdb_adm  identified by "ZZ0r_cle#1";
+create pluggable database PDB2 ADMIN USER pdb_adm identified by "ZZ0r_cle#1";
 alter pluggable database PDB2 open;
 alter session set container=PDB2;
 create tablespace TBS2 datafile size 1G;
